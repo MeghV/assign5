394f1f868ad78553cffcbb2131c28c084774982b
Megh Vakharia <meghhv@gmail.com>
2016-05-09 19:54:43 -0700

    add zobrist hash function, hash table for previous states

--- tanimotosaurusrex_BC_Player.py
+++ tanimotosaurusrex_BC_Player.py
@@ -1,3 +1,5 @@
+from random import *
+
 BLACK = 0
 WHITE = 1
 
@@ -20,17 +22,16 @@ def parse(bs): # bs is board string
       b[iy][jx] = INIT_TO_CODE[rss[jx]]
   return b
 
-INITIAL = parse('''
+INITIAL_UPDATE = parse('''
 c l i w k i l f
-p p p p p p p p
-- - - - - - - -
+p p p p p p p -
+- - - - - - - p
 - - - - - - - -
 - - - - - - - -
 - - - - - - - -
 P P P P P P P P
 F L I W K I L C
 ''')
-
 	
 class BC_state:
   def __init__(self, old_board=INITIAL, whose_move=WHITE):
@@ -75,6 +76,96 @@ def nickname():
 #   in future versions of this assignment.) The timeLimit represents the number of milliseconds available for computing and returning the move.
 # The newRemark to be returned must be a string. During a game, the strings from your agent and 
 # its opponent comprise a dialog. (However, you may simply return a fixed string, such as "Your move!" For extra credit, you may make this more elaborate and context-sensitive, commenting on the current state or the direction in which the game seems to be heading.)
+
+# 14 x 64 table to represent the different pieces
+# (7 on each side) as well as the different positions
+# (8x8 == 64 total)
+PIECES = 14
+POSITIONS = 64
+
+# Adds random values to the 14 x 64 ZOBRIST_NUM_TABLE table 
+# representing all 14 black & white pieces and the 64 positions
+# they can be in. # 64-bit integers are recommended for Chess 
+# as the collision probability is ~1 in 4 billion positions
+ZOBRIST_NUM_TABLE = [ [ randint(0, 9223372036854775807) ] * POSITIONS ] * PIECES
+
+# Stores previous states keyed by their Zobrist hash values.
+# The value of each state is represented as follows:
+# { evaluation_value: 0, ply: 0}
+# "evaluation_value" is the previously calculated static evaluation 
+# function value and "ply" is the ply at which the value was
+# calculated. 
+PREVIOUS_STATES = {}
+
+
+# def init_zobrist_table():
+#   global ZOBRIST_NUM_TABLE
+#   for i in range(PIECES):
+#     for j in range(POSITIONS):
+#       ZOBRIST_NUM_TABLE[i][j] = randint(0, 9223372036854775807)
+
+def calculate_zobrist_hash(state):
+  '''
+  Calculates the Zobrist hash of a state by XORing together all of
+  the random values which represent each piece and position to 
+  produce a hash representing the state. 
+  '''
+  global ZOBRIST_NUM_TABLE
+  hash = 0
+  for row_idx, row in enumerate(state):
+    for piece_idx, piece in enumerate(row):
+      # the numbers 2 - 15 represent black / white pieces on the
+      # gameboard, so subtracting by 2 can represent indices (0 - 13) 
+      # in the ZOBRIST_NUM_TABLE
+      piece_hash_index = piece - 2
+      # ensure we're not dealing with an empty spot
+      if piece_hash_index >= 0:
+        piece_col = (row_idx + 1) * (piece_idx + 1) - 1
+        hash ^= ZOBRIST_NUM_TABLE[piece_hash_index][piece_col]
+  return hash
+        
+def store_state_value(zobrist_hash, evaluation_value, ply=0):
+  '''
+  Caches the static evaluation function value for a unique state
+  to avoid recomputing evaluation values for transpositions in 
+  future searches. The ply at which the static evaluation value 
+  was calculated is also stored.
+  '''
+  global PREVIOUS_STATES
+  # ensures duplicate values aren't stored
+  if zobrist_hash not in PREVIOUS_STATES:
+    PREVIOUS_STATES[zobrist_hash] =\
+      {"evaluation_value": evaluation_value,\
+       "ply": ply}
+
+def get_state_value(zobrist_hash):
+  ''' 
+  Returns the static evaluation value of a previously encountered
+  state. If the state doesn't exist, a value of -1 is returned.
+  '''
+  if zobrist_hash in PREVIOUS_STATES:
+    return PREVIOUS_STATES[zobrist_hash]
+  return -1
+
+def staticEvalBasic(state):
+  state_zobrist_hash = calculate_zobrist_hash(state)
+  state_eval_value = get_state_value(state_zobrist_hash)
+  if state_eval_value == -1:
+    # if the static eval value hasn't been calculated before
+    state_eval_value = 0
+    store_state_value(state_zobrist_hash, state_eval_value)
+  # else return the previously calculated static eval value
+  return state_eval_value
+
+
+def pretty_print_state(state):
+  '''
+  Prints each row in a state on its own line.
+  Used for debugging purposes.
+  '''
+  for row in state:
+    print(row)
+
 
 # staticEval(state). This function will perform a static evaluation of the given state. 
 # The value returned should be high if the state is good for WHITE and low if the state is 
